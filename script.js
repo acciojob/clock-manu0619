e6623b2f2213207a00a4aed38c29f0fd35ab3d3c
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-09 18:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let a=document.querySelector("#timer");
+let time=new Date();
+setInterval(()=>{
+	time=new Date();
+
+	a.innerHTML=time.toLocaleString();
+},1000);
